bc41ba5dd898d36be913a0867249e090453c466d
coco3427 <122467714+coco3427@users.noreply.github.com>
2024-09-24 00:08:35 +0000

    almost done

--- memory-user.c
+++ memory-user.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char *argv[]) {
-int x = atoi(argv[1]);
-int *mem = malloc(x * 1,048,576);//1,048,576 bits in a megabyte
-while{
-    for (int i = 0; i < (x*1,048,576); i++)
+    if (argc<2){
+        fprintf(stderr, "not enough arguments bozo \n");
+        exit(EXIT_FAILURE);
+    }
+int x = atoi(argv[1])*1048576;//1,048,576 bits in a megabyte
+int leng = (int)(x / sizeof(int));
+int *mem = malloc(x);
+while(1){
+    for (int i = 0; i < (leng); i++)
             mem[i] += 1;
 }
+
+free(mem);
 }
